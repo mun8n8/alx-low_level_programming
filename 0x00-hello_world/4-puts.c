e0dba2af1878ed854e332ae6e12c8bf72a5b7cf5
mun8n8 <austinmunene90@gmail.com>
2024-03-24 01:15:17 -0700

    Add 4-puts.c program4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    /* Use the puts function to print the specified string */
-    puts("Programming is like building a multilingual puzzle");
+	/* Use the puts function to print the specified string */
+	puts("Programming is like building a multilingual puzzle");
 
-    /* End the program with the value 0 */
-    return 0;
+	/* End the program with the value 0 */
+	return (0);
 }
