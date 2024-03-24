5197b36a7b746b809fea9b2a81af10e75586e530
mun8n8 <austinmunene90@gmail.com>
2024-03-24 02:39:04 -0700

    Add 101-quote.c program3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <unistd.h>
+/**
+ * main - Prints a quote to standard error
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-write(1, "UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity\n", 98);
+write(1, "UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity\n", 100);
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
 return (1);
 }
