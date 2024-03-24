bc6c311cf4077300fcf9c40dcc4eb9ac0e911a39
mun8n8 <austinmunene90@gmail.com>
2024-03-24 02:45:59 -0700

    Add 101-quote.c program4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-write(1, "UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity\n", 100);
+write(1, "UNIX is basically a simple operating system, but you have\n"
+"to be a genius to understand the simplicity\n", 122);
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
 return (1);
 }
